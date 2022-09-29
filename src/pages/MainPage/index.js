676ce72c4f34a2b69e8702b6be3b5a1299afc663
Nauryzbay <front@astana-creative.kz>
2022-09-29 10:57:37 +0600

    changed main page

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,13 +1,22 @@
 import Cards from "../../components/Cards";
 import LeftTable from "../../components/LeftTable";
 import "../../styles/main-page.css";
-import { getCards } from "../../components/Services";
+import { getCards } from "../../services";
+import { useEffect, useState } from "react";
 const MainPage = ({ date }) => {
+  const [cardsList, setCardsList] = useState([]);
+  useEffect(() => {
+    const handleCompany = async () => {
+      const response = await getCards(date);
+      setCardsList(response);
+    };
+    handleCompany();
+  }, []);
   return (
     <div className="container">
       <div className="page__wrapper">
-        <LeftTable date={date} />
-        <Cards date={date} />
+        <LeftTable date={date} cards={cardsList} />
+        <Cards date={date} cards={cardsList} />
       </div>
     </div>
   );
