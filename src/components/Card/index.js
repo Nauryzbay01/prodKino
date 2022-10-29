f487ef910f6912502d8898f44743351888ab2b09
Nauryzbay <front@astana-creative.kz>
2022-10-29 10:43:17 +0600

    lknfvdnfl

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,6 @@
 import { sliceTime } from "../../utils";
 const Card = ({ title, times, image }) => {
-  const renderTimes = times?.map(({ session }) => {
-
+  const renderTimes = times?.slice(0, 12).map(({ session }) => {
     return (
       <div key={session.id} className="card__time">
         {sliceTime(session?.session_date_tz)}
